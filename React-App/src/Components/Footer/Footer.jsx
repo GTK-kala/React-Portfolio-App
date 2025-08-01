416ec38cdf8ea5d9110bd17dd1b605f35ebb81cc
GTK-kala <gtkkala1110@gmail.com>
2025-08-01 16:14:41 +0300

    Ninth commit

--- React-App/src/Components/Footer/Footer.jsx
+++ React-App/src/Components/Footer/Footer.jsx
@@ -1,18 +1,13 @@
-
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer" id="footer">
-      <div className="footer-content">
-        <div className="footer-content-left">
-          <h1>KHALID</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-            natus debitis ad consequatur? Amet quae, recusandae quas
-            consectetur, quasi culpa ducimus qui quaerat officiis neque, ipsum
-            corporis veniam ad ullam.
-          </p>
+      <div className="footer-one">
+        <p>&copy; {new Date().getFullYear()} Gtkkala. All Rights Reserved</p>
+      </div>
+      <div className="footer-two">
+        <div className="footer-icons">
           <div className="footer-social-icons">
             <a
               href="https://github.com/GTK-kala"
@@ -63,35 +58,7 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div className="footer-content-center">
-          <h2>COMPANY</h2>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Privacy Policy</a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-content-right">
-          <h2>CONTACT US</h2>
-          <ul>
-            <li>+251-910-0907-54</li>
-            <li>gtkkala1110@gmail.com</li>
-          </ul>
-        </div>
       </div>
-      <hr />
-      <p className="footer-copyright">
-        &Copy; {new Date().getFullYear()} GtkKala. All rights reserved.
-      </p>
     </div>
   );
 };
