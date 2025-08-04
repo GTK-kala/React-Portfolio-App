fba9bd785a56c30a4e66004b85f7d3f227944544
GTK-kala <gtkkala1110@gmail.com>
2025-08-04 17:49:46 +0300

    Twenty-one commit

--- React-App/src/Page/Contact/ContactPage.jsx
+++ React-App/src/Page/Contact/ContactPage.jsx
@@ -1,50 +1,71 @@
 import { CiLocationOn } from "react-icons/ci";
 import { IoCallOutline } from "react-icons/io5";
 import { MdOutlineMail } from "react-icons/md";
-import './Contact.css'
+import "./Contact.css";
 
 const ContactPage = () => {
   return (
     <div className="contact-page">
-        <h1>Contact</h1>
-        <p className="contact-description">
-          If you have any questions or feedback, feel free to reach out to us at
-        </p>
-        <div className="address-info">
-           
-           <div className="contact-info">          
-           <p>
-            <span className="icon"><CiLocationOn /></span>
-            <strong>Address</strong> <br /> 
-             <span className="address">Addis Ababa , Ethiopia</span> 
-           </p>
-          
-           <p>
-            <span className="icon"><IoCallOutline /></span>
-              <strong>Call Us</strong> <br /> 
-              <span className="phone">+251 909 100 754</span>
-           </p>
-           <p>
-            <span className="icon"><MdOutlineMail /></span>
-            <strong>Email Us</strong> <br /> 
-             <span className="email">gtkkala1110@gmail.com</span>
-            </p>         
-           </div>
+      <h1>Contact</h1>
+      <p className="contact-description">
+        If you have any questions or feedback, feel free to reach out to us at
+      </p>
+      <div className="address-info">
+        <div className="contact-info">
+          <div className="info-address">
+            <span className="icon">
+              <CiLocationOn />
+            </span>
+            <p>
+              <strong>Address</strong> <br />
+              <span>Addis Ababa , Ethiopia</span>
+            </p>
+          </div>
 
-           <div className="input-info">
-            <form action="">
+          <div className="info-address">
+            <span className="icon">
+              <IoCallOutline />
+            </span>
+            <p>
+              <strong>Call Us</strong> <br />
+              <span>+251 909 100 754</span>
+            </p>
+          </div>
+          <div className="info-address">
+            <span className="icon">
+              <MdOutlineMail />
+            </span>
+            <p>
+              <strong>Email Us</strong> <br />
+              <span>gtkkala1110@gmail.com</span>
+            </p>
+          </div>
+        </div>
+
+        <div className="input-info">
+          <div className="contact-form">
+            <div className="contact-input">
               <label>Your Name</label>
               <input type="text" placeholder="Enter your name" />
+            </div>
+            <div className="contact-input">
               <label>Your Email</label>
               <input type="email" placeholder="Enter your email" />
-              <label>Your Message</label>
-              <textarea placeholder="Enter your message"></textarea>
-              <button type="submit">Send Message</button>
-            </form>
-           </div>
+            </div>
+          </div>
+          <div className="contact-input-subject">
+            <label>Your Subject</label>
+            <input type="text" placeholder="Enter your subject" />
+          </div>
+          <div className="textarea-input">
+            <label>Your Message</label>
+            <textarea placeholder="Enter your message"></textarea>
+            <button type="submit">Send Message</button>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
