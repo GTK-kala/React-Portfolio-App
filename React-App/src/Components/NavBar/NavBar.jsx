228b872be1b9f09ad9694b794b6949ba072b503c
GTK-kala <gtkkala1110@gmail.com>
2025-08-10 21:18:34 +0300

    Fourty-two commit

--- React-App/src/Components/NavBar/NavBar.jsx
+++ React-App/src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom'
 import { RxHamburgerMenu } from "react-icons/rx";
-import { useContext, useState } from 'react';
+import { useContext , useState } from 'react';
 import { DataContext } from '../../Context/DataContext';
 import './NavBar.css'
 
 const NavBar = () => {
+   
+   const [click, setClick] = useState('HOME');
+   const { ToggleHandel , setToggle  , toggle , isOpen , NavLink} = useContext(DataContext);
 
-  const { ToggleHandel , setToggle  , toggle , isOpen} = useContext(DataContext);
 
   return (
     
@@ -18,16 +20,16 @@ const NavBar = () => {
 {/*-----------------------NavBar Center Side---------------------- */}
         <div className={`Right-container ${toggle}`}>
            <ul className={`list-items ${isOpen}`}>
-            <li onClick={ () => setToggle('')}  className={`list-item`}>
+            <li onClick={ () => {setToggle('')  , setClick('HOME')}}  className={`list-item ${click === 'HOME' ? 'active' : ''}`}>
                 <Link to='/'>HOME</Link>
             </li>
-            <li onClick={ () => setToggle('')} className={`list-item`}>
+            <li onClick={ () => {setToggle(''), setClick('ABOUT') }} className={`list-item ${click === 'ABOUT' ? 'active' : ''}`}>
                 <Link to='/About'>ABOUT</Link>
             </li>
-            <li onClick={ () => setToggle('')} className={`list-item`}>
+            <li onClick={ () =>{ setToggle('') , setClick('PROJECTS') }} className={`list-item ${click === 'PROJECTS' ? 'active' : ''}`}>
                 <Link to="/projects">PROJECTS</Link>
             </li>
-            <li onClick={ () => setToggle('')} className={`list-item`}>
+            <li onClick={ () => { setToggle('') , setClick('CONTACT') }} className={`list-item ${click === 'CONTACT' ? 'active' : ''}`}>
                 <Link to="/contact">CONTACT</Link>
             </li>
            </ul>
